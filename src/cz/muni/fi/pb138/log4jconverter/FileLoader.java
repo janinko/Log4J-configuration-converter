c5245c83e55a2a507f4e92089cda0d84f305a21a
5Keyem <5Keyem@gmail.com>
2012-04-24 17:09:18 +0200

    loading file and finding type

--- src/cz/muni/fi/pb138/log4jconverter/FileLoader.java
+++ src/cz/muni/fi/pb138/log4jconverter/FileLoader.java
@@ -6,6 +6,7 @@ package cz.muni.fi.pb138.log4jconverter;
 
 import java.util.Properties;
 import org.w3c.dom.Document;
+import org.apache.commons.io.FilenameUtils;
 
 /**
  * This class represents basic operations for loading
@@ -20,10 +21,29 @@ public class FileLoader {
         XML, PROPERTIES, OTHER
     }
     
+    private Document xmlDoc;
+    private Properties propertiesDoc;
+    private String nameOfFile;
+
+    public FileLoader(String nameOfFile) {
+        if (nameOfFile == null) {
+            throw new NullPointerException("No name of file");
+        }
+        if ("".equals(nameOfFile)) {
+            throw new IllegalArgumentException("name is empty");
+        }  
+        this.nameOfFile = nameOfFile;
+    }
     
     
     public Type getType() {
         // TODO
+        String extension = FilenameUtils.getExtension(nameOfFile);
+        if (extension.equals("xml")) {
+            return Type.XML;
+        } else if (extension.equals("properties")) {
+            return Type.PROPERTIES;
+        }
         return Type.OTHER;
     }
     
@@ -34,13 +54,13 @@ public class FileLoader {
      */
     public Document getDOM() {
         // TODO
-        return null;
+        return xmlDoc;
     }
     
     
     public Properties getProperties() {
         // TODO
-        return null;
+        return propertiesDoc;
     }
     
 }
