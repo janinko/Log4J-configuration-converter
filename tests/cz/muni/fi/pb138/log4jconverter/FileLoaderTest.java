f1feed34366c93041f420018ea13c06b62ba716f
5Keyem <5Keyem@gmail.com>
2012-04-24 17:10:19 +0200

    tests for FileLoader

---
+++ tests/cz/muni/fi/pb138/log4jconverter/FileLoaderTest.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.log4jconverter;
+
+/**
+ *
+ * @author fivekeyem
+ */
+public class FileLoaderTest {
+    
+    
+    
+    
+}
