3957abc4ed1a9b17f4de2c2958b9eaa02987dd77
5Keyem <5Keyem@gmail.com>
2012-04-24 17:09:36 +0200

    demo and main created

---
+++ src/cz/muni/fi/pb138/log4jconverter/Demo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.log4jconverter;
+
+/**
+ *
+ * @author fivekeyem
+ */
+public class Demo {
+    
+            
+}
--- src/cz/muni/fi/pb138/log4jconverter/Main.java
+++ src/cz/muni/fi/pb138/log4jconverter/Main.java
@@ -2,4 +2,9 @@ package cz.muni.fi.pb138.log4jconverter;
 
 public class Main {
 
+    public static void main(String[] args) {
+        FileLoader fl = new FileLoader("neco.xmlsd");
+        System.out.println(fl.getType());
+        
+    }
 }
