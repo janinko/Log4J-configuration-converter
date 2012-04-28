c02e9bef08286535dd97b3dbf68225662a011aaa
Admin <Admin@147.251.76.185>
2012-04-28 17:46:17 +0200

    first implementation of class Category

--- src/cz/muni/fi/pb138/log4jconverter/Logger.java
+++ src/cz/muni/fi/pb138/log4jconverter/Logger.java
@@ -10,31 +10,7 @@ import java.util.HashSet;
  *
  * @author Admin
  */
-/**<h3>Configuring loggers</h3>
 
-    <p>The syntax for configuring the root logger is:
-    <pre>
-      log4j.rootLogger=[level], appenderName, appenderName, ...
-    </pre>
-
-    <p>This syntax means that an optional <em>level</em> can be
-    supplied followed by appender names separated by commas.
-
-    <p>The level value can consist of the string values OFF, FATAL,
-    ERROR, WARN, INFO, DEBUG, ALL or a <em>custom level</em> value. A
-    custom level value can be specified in the form
-    <code>level#classname</code>.
-
-    <p>If a level value is specified, then the root level is set
-    to the corresponding level.  If no level value is specified,
-    then the root level remains untouched.
-
-    <p>The root logger can be assigned multiple appenders.
-
-    <p>Each <i>appenderName</i> (separated by commas) will be added to
-    the root logger. The named appender is defined using the
-    appender syntax defined above.
-    */
 
 
 
