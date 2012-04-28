dde4178776ab63f19b07c76f2132d14af2e47140
Admin <Admin@147.251.76.185>
2012-04-28 16:27:55 +0200

    first implementation of class RootLogger and added empty class Appender

---
+++ src/cz/muni/fi/pb138/log4jconverter/RootLogger.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.log4jconverter;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Admin
+ */
+ /**  <p>The syntax for configuring the root logger is:
+    <pre>
+      log4j.rootLogger=[level], appenderName, appenderName, ...
+    </pre>
+
+    <p>This syntax means that an optional <em>level</em> can be
+    supplied followed by appender names separated by commas.
+
+    <p>The level value can consist of the string values OFF, FATAL,
+    ERROR, WARN, INFO, DEBUG, ALL or a <em>custom level</em> value. A
+    custom level value can be specified in the form
+    <code>level#classname</code>.
+
+    <p>If a level value is specified, then the root level is set
+    to the corresponding level.  If no level value is specified,
+    then the root level remains untouched.
+
+    <p>The root logger can be assigned multiple appenders.
+
+    <p>Each <i>appenderName</i> (separated by commas) will be added to
+    the root logger. The named appender is defined using the
+    appender syntax defined above. */
+public class RootLogger {
+    
+    public enum Levels{OFF, FATAL,
+    ERROR, WARN, INFO, DEBUG, ALL
+    }
+    
+    private Levels level;
+    private HashSet<Appender> appenderNames;
+
+    public HashSet<Appender>  getAppenderNames() {
+        return appenderNames;
+    }
+
+    public void setAppenderNames(HashSet<Appender> appenderNames) {
+        this.appenderNames = appenderNames;
+    }
+
+    public Levels getLevel() {
+        return level;
+    }
+
+    public void setLevel(Levels level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "RootLogger{" + "appenderNames=" + appenderNames + '}';
+    }
+    
+ 
+    
+    
+
+    
+}
