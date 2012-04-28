6ef7eecb42bba50cbe956651b89ebcd8c93aa36b
Jonge <david.jongepier@gmail.com>
2012-04-28 18:47:39 +0200

    Some tests; added Renderer empty class to avoid errors

---
+++ src/cz/muni/fi/pb138/log4jconverter/Renderer.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pb138.log4jconverter;
+
+/**
+ *
+ * @author Jonge
+ */
+public class Renderer {
+    
+}
--- test/cz/muni/fi/pb138/log4jconverter/AppenderTest.java
+++ test/cz/muni/fi/pb138/log4jconverter/AppenderTest.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pb138.log4jconverter;
 
+import java.util.HashMap;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -9,22 +10,31 @@ import static org.junit.Assert.*;
  */
 public class AppenderTest {
     
-    public AppenderTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+    @Test
+    public void AppenderSetGetTest() {
+        Appender appender1 = null;
+        Appender appender2 = null;
+        
+        // Name test
+        appender1.setAppenderName("Appender1");
+        appender2.setAppenderName("Appender2");
+        assertEquals(appender1.getAppenderName(), "Appender1");
+        assertEquals(appender1.getAppenderName(), "Appender2");
+        
+        appender1.setAppenderName("");
+        appender2.setAppenderName(null);
+        assertEquals(appender1.getAppenderName(), "");
+        assertNull(appender2.getAppenderName());
+        
+        // File size
+        appender1.setMaxFileSize("20KB");
+        appender2.setMaxFileSize("30KB");
+        assertEquals(appender1.getMaxFileSize(), "20KB");
+        assertEquals(appender1.getMaxFileSize(), "30KB");
+        
+        appender1.setMaxFileSize("");
+        appender2.setMaxFileSize(null);
+        assertEquals(appender1.getMaxFileSize(), "");
+        assertNull(appender2.getMaxFileSize());
     }
 }
