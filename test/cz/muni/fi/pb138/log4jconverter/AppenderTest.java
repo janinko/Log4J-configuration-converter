d9a5d59846266cdde36a68b096c5120580ede097
Jonge <david.jongepier@gmail.com>
2012-04-28 18:10:07 +0200

    First implementation of tests for Log4J handling classes

---
+++ test/cz/muni/fi/pb138/log4jconverter/AppenderTest.java
@@ -0,0 +1,30 @@
+package cz.muni.fi.pb138.log4jconverter;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jonge
+ */
+public class AppenderTest {
+    
+    public AppenderTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
