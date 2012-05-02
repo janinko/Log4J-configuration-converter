3f26bdbe5393dea500946e5b4720f48776ed3fac
Steve <Admin@147.251.76.185>
2012-05-02 22:49:43 +0200

    complete class Filter according to log4j DTD

--- src/cz/muni/fi/pb138/log4jconverter/configuration/Filter.java
+++ src/cz/muni/fi/pb138/log4jconverter/configuration/Filter.java
@@ -4,10 +4,38 @@
  */
 package cz.muni.fi.pb138.log4jconverter.configuration;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Admin
  */
 public class Filter {
+    
+       //required
+    private String className;
+    //optional
+    private HashMap<String,String> params;
+
+    public Filter(String className) {
+        this.className = className;
+        this.params = new HashMap<String,String>();
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public HashMap<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(HashMap<String, String> params) {
+        this.params = params;
+    }
     
 }
