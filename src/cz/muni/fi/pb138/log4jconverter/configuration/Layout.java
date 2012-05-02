f6c995ea4cea2848214a4545a105a66d453d61b4
Steve <Admin@147.251.76.185>
2012-05-02 22:43:52 +0200

    complete class Layout according to log4j DTD

--- src/cz/muni/fi/pb138/log4jconverter/configuration/Layout.java
+++ src/cz/muni/fi/pb138/log4jconverter/configuration/Layout.java
@@ -4,10 +4,46 @@
  */
 package cz.muni.fi.pb138.log4jconverter.configuration;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Admin
  */
 public class Layout {
+    
+    //required
+    private String className;
+    //optional
+    private HashMap<String,String> params;
+
+    public Layout(String className) {
+        this.className = className;
+        this.params = new HashMap<String,String>();
+    }
+    
+    
+    
+    
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public HashMap<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(HashMap<String, String> params) {
+        this.params = params;
+    }
+    
+    // layout moze byt iba jeden cize equals a hashcode si myslim ze su zbytocne
+    
+    
     
 }
