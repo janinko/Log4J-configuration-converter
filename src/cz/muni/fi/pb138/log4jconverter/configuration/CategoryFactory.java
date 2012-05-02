dc952fcdd28059475930ae23e49320800f444349
Steve <Admin@147.251.76.185>
2012-05-03 01:28:13 +0200

    complete impl class CattegoryFactory,LoggerFactory according to DTD log4j and small change in class Logger

---
+++ src/cz/muni/fi/pb138/log4jconverter/configuration/CategoryFactory.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.log4jconverter.configuration;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Admin
+ */
+public class CategoryFactory {
+    //required
+    private String className;
+    //optional
+    private HashMap<String,String> params = new HashMap<String,String>();
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public HashMap<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(HashMap<String, String> params) {
+        this.params = params;
+    }
+    
+    
+    
+    
+}
