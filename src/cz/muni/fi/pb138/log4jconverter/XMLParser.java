ff00f9d0472345b8054581d3b2af291bc7842191
Jonge <david.jongepier@gmail.com>
2012-05-04 12:29:02 +0200

    XMLParser: writeAllXML() method

--- src/cz/muni/fi/pb138/log4jconverter/XMLParser.java
+++ src/cz/muni/fi/pb138/log4jconverter/XMLParser.java
@@ -1,8 +1,16 @@
 package cz.muni.fi.pb138.log4jconverter;
 
-import org.w3c.dom.Document;
-
 import cz.muni.fi.pb138.log4jconverter.configuration.Configuration;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
 
 public class XMLParser implements Parser {
 	
@@ -10,31 +18,33 @@ public class XMLParser implements Parser {
         private Configuration configuration;
 	
 	public XMLParser(Document document){
-		this.doc = document;
-                this.configuration = null;
+            this.doc = document;
+            this.configuration = null;
 	}
         
-        
-        /*
-         * Helpful for development
-         */
-        public void writeAllXML() {
-            // TODO
+        public void writeAllXML(URI output) throws TransformerConfigurationException, TransformerException {
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(output.toString());
+            transformer.transform(source, result);
         }
         
+        public void writeAllXML(File output) throws TransformerConfigurationException, TransformerException {
+            writeAllXML(output.toURI());
+        }
         
         public void parseXML() {
             // TODO
         }
-        
 
 	@Override
 	public Configuration parse() {
-		if(configuration == null){
-			configuration = new Configuration();
-			parseXML();
-		}
-		return configuration;
+            if(configuration == null){
+                configuration = new Configuration();
+                parseXML();
+            }
+            return configuration;
 	}
 
 }
