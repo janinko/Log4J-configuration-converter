babb2183fd0551a713eef69a287d720a3b779bd9
Steve <Admin@147.251.76.185>
2012-05-06 12:07:52 +0200

    Moved serialization from Configuration to main

--- src/cz/muni/fi/pb138/log4jconverter/Main.java
+++ src/cz/muni/fi/pb138/log4jconverter/Main.java
@@ -1,11 +1,20 @@
 package cz.muni.fi.pb138.log4jconverter;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.Properties;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 
 import org.apache.log4j.PropertyConfigurator;
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import cz.muni.fi.pb138.log4jconverter.InputLoader.Type;
@@ -62,6 +71,27 @@ public class Main {
         */
                
     }
+    
+    // nvm ci sa to hodi do tejto classy
+    private  void serializetoXML(URI output, Document doc)
+           throws IOException, TransformerConfigurationException, TransformerException {
+       // Vytvorime instanci tovarni tridy
+       TransformerFactory factory = TransformerFactory.newInstance();
+       // Pomoci tovarni tridy ziskame instanci tzv. kopirovaciho transformeru
+       Transformer transformer = factory.newTransformer();
+       // Vstupem transformace bude dokument v pameti
+       DOMSource source = new DOMSource(doc);
+       // Vystupem transformace bude vystupni soubor
+       StreamResult result = new StreamResult(output.toString());
+       // Provedeme transformaci
+       transformer.transform(source, result);
+   }
+
+   private void serializetoXML(File output, Document doc) throws IOException,
+           TransformerException {
+       serializetoXML(output.toURI(), doc);
+   }
+    
 
     static final String        OUTL = "--out";
     static final String        OUTS = "-o";
