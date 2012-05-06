8f07adbc3fa1c3820597d2d117fa42d0c0bc9015
Steve <Admin@147.251.76.185>
2012-05-06 05:13:24 +0200

    added 2 serialize method for XML to Configuration class

--- src/cz/muni/fi/pb138/log4jconverter/configuration/Configuration.java
+++ src/cz/muni/fi/pb138/log4jconverter/configuration/Configuration.java
@@ -1,11 +1,20 @@
 package cz.muni.fi.pb138.log4jconverter.configuration;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Writer;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.HashSet;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -229,6 +238,25 @@ public class Configuration implements AbstractModel {
     @Override
     public String toString() {
         return root.toString();
+    }
+    // nvm ci sa to hodi do tejto classy
+     private  void serializetoXML(URI output)
+            throws IOException, TransformerConfigurationException, TransformerException {
+        // Vytvorime instanci tovarni tridy
+        TransformerFactory factory = TransformerFactory.newInstance();
+        // Pomoci tovarni tridy ziskame instanci tzv. kopirovaciho transformeru
+        Transformer transformer = factory.newTransformer();
+        // Vstupem transformace bude dokument v pameti
+        DOMSource source = new DOMSource(doc);
+        // Vystupem transformace bude vystupni soubor
+        StreamResult result = new StreamResult(output.toString());
+        // Provedeme transformaci
+        transformer.transform(source, result);
+    }
+
+    private void serializetoXML(File output) throws IOException,
+            TransformerException {
+        serializetoXML(output.toURI());
     }
 
 }
