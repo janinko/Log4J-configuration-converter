9650ba98c483fe4c91657928098460d1fa3703f1
Steve <Admin@147.251.76.185>
2012-05-06 03:28:22 +0200

    Implementation of Method PrintXML in classes Filter,Layout

--- src/cz/muni/fi/pb138/log4jconverter/configuration/Filter.java
+++ src/cz/muni/fi/pb138/log4jconverter/configuration/Filter.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.pb138.log4jconverter.configuration;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -10,28 +11,29 @@ import org.w3c.dom.Element;
  */
 public class Filter implements Comparable<Filter> {
     //required
+
     private String className;
     //optional
-    private HashMap<String,String> params;
+    private HashMap<String, String> params;
     //used in Properties to distinguish filters and to order them
     private String name;
 
     public Filter() {
-        this.params = new HashMap<String,String>();
+        this.params = new HashMap<String, String>();
     }
-    
-    public Filter(Filter f) {
-    	className = f.className;
-    	params = f.params;
-    	name = f.name;
-	}
 
-	public String getName(){
-    	return name;
+    public Filter(Filter f) {
+        className = f.className;
+        params = f.params;
+        name = f.name;
     }
-    
-    public void setName(String name){
-    	this.name = name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getClassName() {
@@ -42,9 +44,9 @@ public class Filter implements Comparable<Filter> {
         this.className = className;
     }
 
-	public void addParam(String key, String value) {
-		params.put(key, value);
-	}
+    public void addParam(String key, String value) {
+        params.put(key, value);
+    }
 
     public HashMap<String, String> getParams() {
         return params;
@@ -55,53 +57,81 @@ public class Filter implements Comparable<Filter> {
     }
 
     @Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Filter other = (Filter) obj;
-		if (className == null) {
-			if (other.className != null)
-				return false;
-		} else if (!className.equals(other.className))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (params == null) {
-			if (other.params != null)
-				return false;
-		} else if (!params.equals(other.params))
-			return false;
-		return true;
-	}
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Filter other = (Filter) obj;
+        if (className == null) {
+            if (other.className != null) {
+                return false;
+            }
+        } else if (!className.equals(other.className)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (params == null) {
+            if (other.params != null) {
+                return false;
+            }
+        } else if (!params.equals(other.params)) {
+            return false;
+        }
+        return true;
+    }
 
     @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((className == null) ? 0 : className.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((params == null) ? 0 : params.hashCode());
-		return result;
-	}
-
-	@Override
-	public int compareTo(Filter o) {
-		if(name == null || o.name == null){
-			throw new IllegalArgumentException("Uncomparable filters");
-		}
-		return name.compareTo(o.name);
-	}
-
-    void printXML(Document doc, Element appender) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((className == null) ? 0 : className.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((params == null) ? 0 : params.hashCode());
+        return result;
     }
-    
+
+    @Override
+    public int compareTo(Filter o) {
+        if (name == null || o.name == null) {
+            throw new IllegalArgumentException("Uncomparable filters");
+        }
+        return name.compareTo(o.name);
+    }
+
+    void printXML(Document doc, Element elem) {
+        Element filter = doc.createElement("filter");
+
+        filter.setAttribute("class", className);
+
+
+        if (!params.isEmpty()) {
+            Iterator it1 = params.keySet().iterator();
+            Iterator it2 = params.values().iterator();
+            while (it1.hasNext()) {
+                Element param = doc.createElement("param");
+
+                param.setAttribute("name", it1.next().toString());
+                param.setAttribute("value", it2.next().toString());
+                filter.appendChild(param);
+
+            }
+
+        }
+
+
+        elem.appendChild(filter);
+    }
 }
--- src/cz/muni/fi/pb138/log4jconverter/configuration/Layout.java
+++ src/cz/muni/fi/pb138/log4jconverter/configuration/Layout.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.pb138.log4jconverter.configuration;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -41,7 +42,28 @@ public class Layout {
     // layout moze byt iba jeden cize equals a hashcode si myslim ze su zbytocne
 
     void printXML(Document doc, Element appender) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Element layout = doc.createElement("layout");
+      
+        layout.setAttribute("class", className);
+        
+        
+        if (!params.isEmpty()) {
+            Iterator it1 = params.keySet().iterator();
+            Iterator it2 = params.values().iterator();
+            while (it1.hasNext()) {
+                Element param = doc.createElement("param");
+
+                param.setAttribute("name",it1.next().toString());
+                param.setAttribute("value",it2.next().toString());
+                layout.appendChild(param);
+                
+            }
+            
+        }
+        
+        
+        appender.appendChild(layout);
     }
+    
 
 }
