c7f77b86a9db222f2a4d91c81f4c2bd4f9496032
Steve <Admin@147.251.76.185>
2012-05-12 19:49:39 +0200

    create test for parse Conf Attributes,Renderer and Throwable Renderer

--- test/cz/muni/fi/pb138/log4jconverter/XMLParserTest.java
+++ test/cz/muni/fi/pb138/log4jconverter/XMLParserTest.java
@@ -5,10 +5,14 @@
 package cz.muni.fi.pb138.log4jconverter;
 
 import cz.muni.fi.pb138.log4jconverter.configuration.Configuration;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import cz.muni.fi.pb138.log4jconverter.Helper;
+import cz.muni.fi.pb138.log4jconverter.configuration.Renderer;
+import cz.muni.fi.pb138.log4jconverter.configuration.ThrowableRenderer;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -30,16 +34,20 @@ public class XMLParserTest {
     }
     private Document doc;
     private Configuration config;
+    private XMLParser parser;
    
 
     
     @Before
     public void setUp() throws ParserConfigurationException, IOException, SAXException {
-         config = new Configuration();
+        
+         
          File file = new File("../test/cz/muni/fi/pb138/log4jconverter/Configuration.xml");
          DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
          DocumentBuilder db = dbf.newDocumentBuilder();
          this.doc = db.parse(file);
+         this.parser = new XMLParser(doc);
+         config = parser.parse();
     }
 
     @Test
@@ -52,5 +60,52 @@ public class XMLParserTest {
             fail();   
         }
     }
-
+     @Test
+     public void testParseConfigurationAttributes()
+     {
+         
+         assertEquals(config.getThreshold().toString(),"DEBUG");
+         assertEquals(config.getDebug().toString(),"false");
+         assertEquals(config.isReset(),false);
+         
+       
+         
+         
+       
+          
+          
+          
+         
+     }
+      @Test
+     public void testParseRenderer()
+      {
+         for(Renderer r : config.getRenderers())
+         {
+         assertEquals(r.getRenderedClass(),"RenderedClass");
+         assertEquals(r.getRenderingClass(),"RenderingClass");
+         }
+          
+      }
+      @Test
+      public void testParseThrowableRenderer()
+      {
+         ThrowableRenderer throwRen = config.getThrowableRenderer();
+         assertEquals(throwRen.getClassName(),"throwableRenderer.cs");
+         HashSet<String> keys = (HashSet<String>) throwRen.getParams().keySet();
+         HashSet<String> values = (HashSet<String>) throwRen.getParams().values();
+         for(String s : keys)
+         {
+             assertEquals(s,"ParamName");
+         }
+          for(String s : values)
+         {
+             assertEquals(s,"ParamValue");
+         }
+      }
+     
+    
+    
+    
+    
 }
