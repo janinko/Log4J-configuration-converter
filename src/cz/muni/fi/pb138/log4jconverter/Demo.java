469c0c4d9e3b94c86a3b10277db0aaf69b6ad571
5Keyem <5Keyem@gmail.com>
2012-05-13 12:59:09 +0200

    edit Demo2

--- src/cz/muni/fi/pb138/log4jconverter/Demo.java
+++ src/cz/muni/fi/pb138/log4jconverter/Demo.java
@@ -4,11 +4,46 @@
  */
 package cz.muni.fi.pb138.log4jconverter;
 
+import cz.muni.fi.pb138.log4jconverter.configuration.Configuration;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import org.w3c.dom.Document;
+
 /**
  *
  * @author fivekeyem
  */
 public class Demo {
     
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		InputLoader il = new InputLoader("examples/exampleConfigs.properties");
+	
+		Properties p = il.getProperties();
+		
+		p.list(System.out);
+		
+		System.out.println("--------- prop -> abst -> xml .... ---------");
+		
+		PropertiesParser pp = new PropertiesParser(p);
+		
+		Configuration c = pp.parse();
+		
+		Document xml = c.generateXML();
+		
+		Helper.printAllXml(xml.getChildNodes(), 0);
+		
+		System.out.println("---------- xml -> abst -> prop");
+		
+		XMLParser xmlp = new XMLParser(xml);
+		c = xmlp.parse();
+		
+		Properties p2 = c.generateProperties();
+		
+		System.out.println("--------- write prop -------------");
+		
+		p2.list(System.out);
+		
+	}
             
 }
