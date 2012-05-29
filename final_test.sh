ad4ff24d84bcd213e28042eaca0c4f00ea2d5bb5
Jan Brázdil <janinko.g@gmail.com>
2012-05-29 10:49:36 +0200

    bash test and test examples

---
+++ final_test.sh
@@ -0,0 +1,54 @@
+DIRNAME=`dirname $0`
+TESTDIR=`mktemp -d`
+JARFILE=log4jcc-0.6.jar
+
+TESTFILESDIR=$DIRNAME/examples/testExamples/
+
+cp $TESTFILESDIR* $TESTDIR
+
+ARGS="-v"
+
+for a in $TESTFILESDIR*.properties; do
+	java -jar $JARFILE -i $a -o $TESTDIR/`basename $a`.properties -T properties $ARGS
+done
+
+for a in $TESTFILESDIR*.properties; do
+	java -jar $JARFILE -i $a -o $TESTDIR/`basename $a`.xml -T xml $ARGS
+done
+
+for a in $TESTFILESDIR*.xml; do
+	java -jar $JARFILE -i $a -o $TESTDIR/`basename $a`.xml -T xml $ARGS
+done
+
+for a in $TESTFILESDIR*.xml; do
+	java -jar $JARFILE -i $a -o $TESTDIR/`basename $a`.properties -T properties $ARGS
+done
+
+for a in $TESTFILESDIR*.xml; do
+	java -jar $JARFILE -i $TESTDIR/`basename $a`.properties -o $TESTDIR/`basename $a`.properties.xml -T xml $ARGS
+done
+
+for a in $TESTFILESDIR*.properties; do
+	java -jar $JARFILE -i $TESTDIR/`basename $a`.xml -o $TESTDIR/`basename $a`.xml.properties -T properties $ARGS
+done
+
+
+cd $TESTDIR
+
+for a in *.properties; do
+	sort $a | sponge $a;
+done
+
+for a in *[0-9].properties; do
+	cat $a | grep -vE "^(\s*$|#)" | sed 's/\\:/:/;s/, /,/g;s/ *= */=/' | sponge $a;
+done
+
+for a in *[0-9].properties; do
+	diff $a $a.properties -U0 | grep "^[+-]"
+done
+	
+for a in *[0-9].properties; do
+	diff $a $a.xml.properties -U0 | grep "^[+-]"
+done
+	
+
